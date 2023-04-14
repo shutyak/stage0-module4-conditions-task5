c67207a21fab768aaef8d3b05e42ebfa1e707d29
vlshutyak <vl.shutyak@gmail.com>
2023-04-14 22:11:53 +0300

    s0-m4-t5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeap = true;
+                }
+            } else {
+                isLeap = true;
+            }
+        }
+
+        if (isLeap) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
